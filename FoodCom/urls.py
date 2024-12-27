7e74a59da50060dfed97d77b2b4b6aa46b094637
PritPatel1908 <71305381+PritPatel1908@users.noreply.github.com>
2024-12-27 08:33:26 +0530

    Update urls.py

--- FoodCom/urls.py
+++ FoodCom/urls.py
@@ -159,7 +159,7 @@ urlpatterns = [
 
     # Logout
     path('logout', views.logout, name='logout'),
-] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
